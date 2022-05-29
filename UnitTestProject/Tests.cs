dde9c5c47263c9de93927c2b70cee7932fc0dcf9
Vladyslav.Savchenko <Vladyslav.Savchenko@VSAVCHENKO-N>
2022-05-29 17:01:01 +0200

    added the full test on Selenium

--- UnitTestProject/Tests.cs
+++ UnitTestProject/Tests.cs
@@ -1,11 +1,15 @@
 ﻿
 using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 using System;
 
 namespace UnitTestProject
 {
     public class Tests
     {
+        public IWebDriver driver;
+
         [OneTimeSetUp]
         public void OneTimeSetupMethod()
         {
@@ -15,7 +19,15 @@ namespace UnitTestProject
         [SetUp]
         public void SetupMethod()
         {
+            driver = new ChromeDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.Manage().Window.Maximize();
+        }
 
+        [TearDown]
+        public void AfterEachTest()
+        {
+            driver.Quit();
         }
 
         [Category("Smoke")]
@@ -48,6 +60,83 @@ namespace UnitTestProject
         [Test]
         public void CheckIWebDriverAndIWebElements()
         {
+            
+            
+            //arrange
+            driver.Url = "https://kasamba.com";
+
+            //act
+            var linkToSignUp = driver.FindElement(By.CssSelector("#ctl00_Header_LiteHeader1_lnkHeaderRegister"));
+            linkToSignUp.Click();
+
+            var linkToLoginFromRegister = driver.FindElement(By.CssSelector("#siginlink"));
+            linkToLoginFromRegister.Click();
+
+            string expectedResult = "Kasamba - Member Sign in";
+            string actualResult = driver.Title.Trim();
+
+            #region Methods
+            /*
+            linkToLoginFromRegister.Clear(); // clear the input
+            linkToLoginFromRegister.SendKeys("text"); // send text to input
+            linkToLoginFromRegister.FindElement(By.CssSelector(".//test")); // find child in element
+
+            var elementText = linkToLoginFromRegister.Text; // return innerText
+            bool enbaled = linkToLoginFromRegister.Enabled; // is element enabled
+            bool displayed = linkToLoginFromRegister.Displayed; // is element displayed
+
+            
+            elementText = linkToLoginFromRegister.GetAttribute("innerText");
+            elementText = linkToLoginFromRegister.GetAttribute("alt");
+            elementText = linkToLoginFromRegister.GetAttribute("textContent"); // preferable method to get text
+
+            //just for input
+            elementText = linkToLoginFromRegister.GetAttribute("value");
+
+            // for checkbox and radio-buttons
+            bool isSelected = linkToLoginFromRegister.Selected;
+            */
+            #endregion 
+            //assert
+            Assert.AreEqual(expectedResult, actualResult, $"System Expetected result is: {expectedResult}, but Actual result is:{actualResult}");
+            
+        }
+
+        [Test]
+        public void UkrNetSearch()
+        {
+            driver.Url = "https://www.ukr.net/";
+            var homePageHandle = driver.CurrentWindowHandle;
+
+            var searchInput = driver.FindElement(By.Id("search-input"));
+
+            searchInput.SendKeys("Civilization 6");
+            searchInput.Submit();
+
+            var allHandles = driver.WindowHandles;
+            foreach (var handle in allHandles)
+            {
+                if (handle != homePageHandle)
+                    driver.SwitchTo().Window(handle);
+            }
+                        
+            var firstFoundLink = driver.FindElements(By.CssSelector("div>a.gs-title"));
+            var searchResultsPageHandle = driver.CurrentWindowHandle;
+            
+            firstFoundLink[0].Click();
+
+            allHandles = driver.WindowHandles;
+            foreach (var handle in allHandles)
+            {
+                if (handle != homePageHandle && handle != searchResultsPageHandle)
+                    driver.SwitchTo().Window(handle);
+            }
+
+
+            string expectedResult = "Sid Meier’s Civilization® VI on Steam";
+            string actualResult = driver.Title.Trim();
+
+            Assert.AreEqual(expectedResult, actualResult, $"System Expetected result is: {expectedResult}, but Actual result is:{actualResult}");
 
         }
     }
